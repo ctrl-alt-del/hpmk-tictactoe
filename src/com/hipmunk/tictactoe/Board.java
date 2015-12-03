78c5c55e4c1a90817e2f460e05eba813472e6d76
Jiyang Liu <jiyangliu4job@gmail.com>
2015-12-02 22:05:51 -0800

    added and implemented Board class;

---
+++ src/com/hipmunk/tictactoe/Board.java
@@ -0,0 +1,51 @@
+package com.hipmunk.tictactoe;
+
+public class Board {
+    private int mColumn;
+    private int mRow;
+    private int[][] mBoard;
+    private boolean[][] mTaken;
+
+    public Board(int row, int column) {
+        mRow = row;
+        mColumn = column;
+        mBoard = new int[mRow][mColumn];
+        mTaken = new boolean[mRow][mColumn];
+    }
+
+    public Board set(int row, int column, int value) {
+        mBoard[row][column] = value;
+        return this;
+    }
+
+    public int get(int row, int column) {
+        return mBoard[row][column];
+    }
+
+    public void setTaken(int row, int column, boolean taken) {
+        mTaken[row][column] = taken;
+    }
+
+    public boolean isTaken(int row, int column) {
+        return mTaken[row][column];
+    }
+
+    public boolean isWithinBound(int row, int column) {
+        return row >= 0 && row < mRow && column >= 0 && column < mColumn;
+    }
+
+    public boolean isAvailable(int x, int y) {
+        return isWithinBound(x, y) && !isTaken(x, y);
+    }
+
+    public void print() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : mBoard) {
+            for (int item : row) {
+                sb.append(item + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
--- src/com/hipmunk/tictactoe/Main.java
+++ src/com/hipmunk/tictactoe/Main.java
@@ -3,6 +3,8 @@ package com.hipmunk.tictactoe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Board board = new Board(3, 3);
+        board.print();
     }
 }
