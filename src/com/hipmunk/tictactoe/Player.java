7d27f3bc2b9b8e2095a274055bc12bde5efd64ba
Jiyang Liu <jiyangliu4job@gmail.com>
2015-12-03 19:46:33 -0800

    added logic to avoid unnecessary competitive check for the first two steps;

--- src/com/hipmunk/tictactoe/Player.java
+++ src/com/hipmunk/tictactoe/Player.java
@@ -13,7 +13,6 @@ public class Player {
         mRandom = new Random();
         mMarker = marker;
         mMoveCount = 0;
-        mBoardWidth = 3;
     }
 
     /**
@@ -25,31 +24,32 @@ public class Player {
         * 1. check if there is a spot that makes me win
         * 2. check if there is a spot that blocks other from winning
         */
-
-        for (int i = 0; i < board.getRow(); i++) {
-            for (int j = 0; j < board.getColumn(); j++) {
-                if (board.isAvailable(i, j) && BoardUtils.checkWinner(board, i, j, getMarker(), false)) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-
-        final char otherMarker = getMarker() == board.getPlayer().getMarker() ? board.getComputer().getMarker() : board.getPlayer().getMarker();
-        List<Integer[]> availablePoints = new ArrayList<>();
-        for (int i = 0; i < board.getRow(); i++) {
-            for (int j = 0; j < board.getColumn(); j++) {
-                if (board.isAvailable(i, j)) {
-                    availablePoints.add(new Integer[]{i, j});
-                    if (BoardUtils.checkWinner(board, i, j, otherMarker, false)) {
+        if (mMoveCount >= 2) {
+            for (int i = 0; i < board.getRow(); i++) {
+                for (int j = 0; j < board.getColumn(); j++) {
+                    if (board.isAvailable(i, j) && BoardUtils.checkWinner(board, i, j, getMarker(), false)) {
                         return new int[]{i, j};
                     }
                 }
             }
-        }
 
-        if (availablePoints.size() > 0) {
-            Integer[] point = availablePoints.get(mRandom.nextInt(availablePoints.size()));
-            return new int[]{point[0], point[1]};
+            final char otherMarker = getMarker() == board.getPlayer().getMarker() ? board.getComputer().getMarker() : board.getPlayer().getMarker();
+            List<Integer[]> availablePoints = new ArrayList<>();
+            for (int i = 0; i < board.getRow(); i++) {
+                for (int j = 0; j < board.getColumn(); j++) {
+                    if (board.isAvailable(i, j)) {
+                        availablePoints.add(new Integer[]{i, j});
+                        if (BoardUtils.checkWinner(board, i, j, otherMarker, false)) {
+                            return new int[]{i, j};
+                        }
+                    }
+                }
+            }
+
+            if (availablePoints.size() > 0) {
+                Integer[] point = availablePoints.get(mRandom.nextInt(availablePoints.size()));
+                return new int[]{point[0], point[1]};
+            }
         }
 
         int x = mRandom.nextInt(board.getRow());
